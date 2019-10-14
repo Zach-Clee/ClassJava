3276475e22e365bf53786579885f7c77a42d7376
Zach <abc@hotmail.co>
2019-10-14 16:34:43 +0100

    adding radio buttons to calc

--- Menu/src/MainCode.java
+++ Menu/src/MainCode.java
@@ -1,4 +1,6 @@
 import java.awt.Button;
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
 import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.Label;
@@ -9,13 +11,16 @@ import java.awt.event.ActionListener;
 public class MainCode {
 	public static void main (String[] args) {
 		Frame F = new Frame("Main");
+		
+
+		
 		F.setVisible(true);
 		F.setSize(300,300);
 		Button Addition = new Button("Addition");
 		Button Subtraction = new Button("Subtraction");
 		Button Multiplication = new Button("Multiplication");
 		Button Division = new Button ("Division");
-		F.setLayout(new GridLayout(4,1));
+		F.setLayout(new GridLayout(5,1));
 		F.add(Addition);
 		F.add(Subtraction);
 		F.add(Multiplication);
@@ -60,13 +65,14 @@ public class MainCode {
 
 class FrameTemplate{
 	Frame F = new Frame("Main");
-	Button Button1 = new Button();
 	TextField No1, No2, Results;
+	CheckboxGroup group=new CheckboxGroup();
+	Button Button1 = new Button("Operate");
 	public FrameTemplate(){
 		No1 = new TextField();
 		No2 = new TextField();
 		Results = new TextField();
-		F.setLayout(new GridLayout(4,2));
+		F.setLayout(new GridLayout(10,2));
 		
 		F.setSize(300,300);
 		F.add(new Label("First No."));
@@ -74,20 +80,36 @@ class FrameTemplate{
 		F.add(new Label("Second No."));
 		F.add(No2);
 		F.add(new Label());
+		Checkbox Addition = new Checkbox("Addition", group, false);
+		Checkbox Subtraction = new Checkbox("Subtraction", group, false);
+		Checkbox Multiplication = new Checkbox("Multiplication", group, false);
+		Checkbox Division = new Checkbox("Division", group, false);
+		F.add(Addition);
+		F.add(new Label());
+		F.add(Subtraction);
+		F.add(new Label());
+		F.add(Multiplication);
+		F.add(new Label());
+		F.add(Division);
+		F.add(new Label());
 		F.add(Button1);
+		F.add(new Label());
+		F.add(new Label());
+
+		
 		F.add(new Label("Results"));
 		F.add(Results);
 		
 		Button1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (Button1.getLabel() == "Addition" ) {
-			 Results.setText(Integer.toString((Integer.parseInt(No1.getText())) + (Integer.parseInt(No1.getText()))));
-				}else if (Button1.getLabel() == "Subtraction" ) {
-					 Results.setText(Integer.toString((Integer.parseInt(No1.getText())) - (Integer.parseInt(No1.getText()))));
-				}else if (Button1.getLabel() == "Multiplication" ) {
-					 Results.setText(Float.toString((Float.parseFloat(No1.getText())) * (Float.parseFloat(No1.getText()))));
-				}else{
-					 Results.setText(Float.toString((Float.parseFloat(No1.getText())) / (Float.parseFloat(No1.getText()))));
+				if (Addition.getState()){
+			 Results.setText(Integer.toString((Integer.parseInt(No1.getText())) + (Integer.parseInt(No2.getText()))));
+				} else if (Subtraction.getState()){
+			 Results.setText(Integer.toString((Integer.parseInt(No1.getText())) - (Integer.parseInt(No2.getText()))));
+				} else if (Multiplication.getState()){
+					 Results.setText(Integer.toString((Integer.parseInt(No1.getText())) * (Integer.parseInt(No2.getText()))));
+				} else if (Division.getState()){
+					 Results.setText(Float.toString((Float.parseFloat(No1.getText())) / (Float.parseFloat(No2.getText()))));
 				}
 			}
 		});
